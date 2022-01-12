8f78b0d35432203740fba8137ca0770f0c31998b
Antar Martin <antarwritescode@gmail.com>
2022-01-11 20:56:20 -0800

    made changes to file

---
+++ js/questions.js
@@ -0,0 +1,53 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
+    },
+    {
+      title: "The condition in an if/else statement is enclosed within ______.",
+      choices: [
+        "Quotes",
+        "Curly brackets",
+        "Parentheses",
+        "Square brackets"
+      ],
+      answer: "Curley brackets"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store",
+      choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+      answer: "All of the above"
+    },
+    {
+      title: "String values must be enclosed within ______ when being assigned to variables.",
+      choices: [
+        "Commas",
+        "Curly brackets",
+        "Quotes",
+        "Parentheses"
+      ],
+      answer: "Commas"
+    },
+    {
+      title:
+        "Which one of these is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Animation time errors",
+        "Load time errors",
+        "Run time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
+    },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: [
+        "JavaScript",
+        "Terminal/Bash",
+        "For loops",
+        "Console log"
+      ],
+      answer: "Console log"
+    },
+  ];
